0556c1d37a298c6377da4a52e5ec1968bbbffe68
Gabriel C. de Souza <gabrielsouza_1909@hotmail.com>
2024-12-16 11:37:24 -0300

    Cypress Config

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,25 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require("cypress")
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+  reporter: 'cypress-multi-reporters',
+
+  reporterOptions: {
+    reporterEnabled: 'cypress-mochawesome-reporter',
+
+    cypressMochawesomeReporterReporterOptions: {
+      charts: true,
+      reportPageTitle: 'Relatório de testes',
+      embeddedScreenshots: true,
+      inlineAssets: true,
+      saveAllAttempts: false
+    }
   },
-});
+
+  chromeWebSecurity: false,
+
+  e2e: {
+    video: true,
+    videosFolder: 'cypress/videos',
+    setupNodeEvents(on, config) { require('cypress-mochawesome-reporter/plugin')(on) },
+  },
+})
